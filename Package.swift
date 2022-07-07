1931d194ff4ae76f83c970c9a3362a1f4249bea7
Vitaliy Zhukov <vi.zhukov@tinkoff.ru>
2022-07-07 13:01:41 +0700

    dependency

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(name: "EncryptionProvider", path: "https://github.com/ZhuchoG/EncryptionProvider.git")
+        .package(url: "https://github.com/ZhuchoG/EncryptionProvider.git", from: "1.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
